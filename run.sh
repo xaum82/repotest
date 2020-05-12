78aebed79b265148e704da0866515d971ac46208
xaum82 <35361267+xaum82@users.noreply.github.com>
2020-05-12 23:25:03 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,19 @@
+function ExtensionsVSCode(){
+    code --install-extension ms-azuretools.vscode-docker
+    code --install-extension tqrhsn.vscode-docker-registry-explorer
+    code --install-extension msjsdiag.debugger-for-chrome
+    code --install-extension firefox-devtools.vscode-firefox-debug
+    code --install-extension michelemelluso.gitignore
+    code --install-extension jheilingbrunner.vscode-gnupg-tool
+    code --install-extension geyao.html-snippets
+    code --install-extension abusaidm.html-snippets
+    code --install-extension mstuttgart.odoo-scaffold
+    code --insExtensionsVSCodetall-extension ckolkman.vscode-postgres
+    code --install-extension ms-python.python
+    code --install-extension tushortz.python-extended-snippets
+    code --install-extension donjayamanne.python-extension-pack
+    code --install-extension tht13.python
+    code --install-extension cstrap.python-snippets
+    code --install-extension dawhite.mustache
+}
+
